f0046ad195fd6e6e76b337530b2b69e3552687ff
Dejan Bjelic <dejan.bjelic@uns.ac.rs>
2023-04-29 06:25:44 +0200

    Asset: Show edit button on assets of authenticated user

--- warehouse/src/app/component/assets-list/assets-list.component.ts
+++ warehouse/src/app/component/assets-list/assets-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AssetListView } from 'src/app/model/asset-list-view';
 import { AssetService } from 'src/app/service/asset.service';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-assets-list',
@@ -9,9 +10,12 @@ import { AssetService } from 'src/app/service/asset.service';
 })
 export class AssetsListComponent implements OnInit {
 
-  constructor(private assetService: AssetService){}
+  constructor(
+    private assetService: AssetService,
+    public authService: AuthenticationService){}
+
   assets: AssetListView[] = []
-  noAssetsFound: boolean = false
+  assetsExist: boolean  = true
 
   @Input() id?: number = 0
   
@@ -20,9 +24,10 @@ export class AssetsListComponent implements OnInit {
       next: (loadedAssets: AssetListView[]) => 
       {
         this.assets = loadedAssets;
-        if(this.assets.length == 0)
-          this.noAssetsFound = true
-          
+        
+        if(this.assets.length < 1){
+          this.assetsExist = false
+        }  
         console.log(this.assets)
       },
       error: () => {}
